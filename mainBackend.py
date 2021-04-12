a454c10221f860dd13445d7b5f80a2eba8499ae4
YitzchakMeltz <75029003+YitzchakMeltz@users.noreply.github.com>
2021-04-12 20:21:31 +0300

    Connect buttons

--- mainBackend.py
+++ mainBackend.py
@@ -1,5 +1,16 @@
 mathEq=""
 
 def button_click(userClick):
-    mathEq+=userClick
+    global mathEq
+    mathEq += userClick
+    return
+
+def button_equals_click():
+    global mathEq
+    print(mathEq)
+    return
+
+def button_clear_click():
+    global mathEq
+    mathEq=""
     return
